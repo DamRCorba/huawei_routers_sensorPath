8758d7912b888801ae10c19fcb6f7eb7e371861b
Damian Corbalan <damian.corbalan@telefonica.com>
2021-07-01 11:12:55 -0300

    package correction

--- huawei_routers_sensorPath.go
+++ huawei_routers_sensorPath.go
@@ -32,83 +32,83 @@ func GetMessageType(path string) (proto.Message) {
   sensorType := strings.Split(path,":")
   switch sensorType[0] {
   case "huawei-bfd":
-      return &huawei_bfd.Bfd{}
+      return &huaweiV8R10_bfd.Bfd{}
 
   case "huawei-bgp":
     switch sensorType[1] {
     case "ESTABLISHED":
-      return &huawei_bgp.ESTABLISHED{}
+      return &huaweiV8R10_bgp.ESTABLISHED{}
     case "BACKWARD":
-      return &huawei_bgp.BACKWARD{}
+      return &huaweiV8R10_bgp.BACKWARD{}
     }
-    return &huawei_bgp.ESTABLISHED{}
+    return &huaweiV8R10_bgp.ESTABLISHED{}
 
   case "huawei-devm":
-    return &huawei_devm.Devm{}
+    return &huaweiV8R10_devm.Devm{}
 
   case "huawei-driver":
     switch sensorType[1] {
     case "hwEntityInvalid":
-        return &huawei_driver.HwEntityInvalid{}
+        return &huaweiV8R10_driver.HwEntityInvalid{}
     case "hwEntityResume":
-        return &huawei_driver.HwEntityResume{}
+        return &huaweiV8R10_driver.HwEntityResume{}
     case "hwOpticalInvalid":
-      return &huawei_driver.HwOpticalInvalid{}
+      return &huaweiV8R10_driver.HwOpticalInvalid{}
     case "hwOpticalInvalidResume":
-      return &huawei_driver.HwOpticalInvalidResume{}
+      return &huaweiV8R10_driver.HwOpticalInvalidResume{}
     }
-    return &huawei_driver.HwEntityInvalid{}
+    return &huaweiV8R10_driver.HwEntityInvalid{}
 
   case "huawei-ifm":
-    return &huawei_ifm.Ifm{}
+    return &huaweiV8R10_ifm.Ifm{}
 
   case "huawei-isis":
   case "huawei-isiscomm":
-    return &huawei_isiscomm.IsisAdjacencyChange{}
+    return &huaweiV8R10_isiscomm.IsisAdjacencyChange{}
 
   case "huawei-mpls":
-    return &huawei_mpls.Mpls{}
+    return &huaweiV8R10_mpls.Mpls{}
 
   case "huawei-ospfv2":
     switch sensorType[1] {
     case "ospfNbrStateChange":
-      return &huawei_ospfv2.OspfNbrStateChange{}
+      return &huaweiV8R10_ospfv2.OspfNbrStateChange{}
     case "ospfVirtNbrStateChange":
-      return &huawei_ospfv2.OspfVirtNbrStateChange{}
+      return &huaweiV8R10_ospfv2.OspfVirtNbrStateChange{}
     }
-    return &huawei_ospfv2.OspfNbrStateChange{}
+    return &huaweiV8R10_ospfv2.OspfNbrStateChange{}
 
   case "huawei-ospfv3":
-    return &huawei_ospfv3.Ospfv3NbrStateChange{}
+    return &huaweiV8R10_ospfv3.Ospfv3NbrStateChange{}
 
   case "huawei-qos":
-    return &huawei_qos.Qos{}
+    return &huaweiV8R10_qos.Qos{}
 
   case "huawei-sem":
     switch sensorType[1] {
     case "hwCPUUtilizationResume":
-      return &huawei_sem.HwStorageUtilizationResume{}
+      return &huaweiV8R10_sem.HwStorageUtilizationResume{}
     case "hwCPUUtilizationRisingAlarm":
-      return &huawei_sem.HwCPUUtilizationRisingAlarm{}
+      return &huaweiV8R10_sem.HwCPUUtilizationRisingAlarm{}
     case "hwStorageUtilizationResume":
-      return &huawei_sem.HwStorageUtilizationResume{}
+      return &huaweiV8R10_sem.HwStorageUtilizationResume{}
     case "hwStorageUtilizationRisingAlarm":
-      return &huawei_sem.HwStorageUtilizationRisingAlarm{}
+      return &huaweiV8R10_sem.HwStorageUtilizationRisingAlarm{}
       }
-    return &huawei_sem.HwStorageUtilizationResume{}
+    return &huaweiV8R10_sem.HwStorageUtilizationResume{}
 
   case "huawei-telmEmdi":
   case "huawei-emdi":
-    return &huawei_telemEmdi.TelemEmdi{}
+    return &huaweiV8R10_telemEmdi.TelemEmdi{}
 
   case "huawei-trafficmng":
-    return &huawei_trafficmng.Trafficmng{}
+    return &huaweiV8R10_trafficmng.Trafficmng{}
 
   default:
     fmt.Println("Error Sensor Desconocido")
-    return &huawei_devm.Devm{}
+    return &huaweiV8R10_devm.Devm{}
   }
-    return &huawei_devm.Devm{}
+    return &huaweiV8R10_devm.Devm{}
 }
 
 
@@ -129,14 +129,14 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-bgp":
     switch splited[1] {
     case "ESTABLISHED":
-      fooType := reflect.TypeOf(huawei_bgp.ESTABLISHED{})
+      fooType := reflect.TypeOf(huaweiV8R10_bgp.ESTABLISHED{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
     break;
     case "BACKWARD":
-      fooType := reflect.TypeOf(huawei_bgp.BACKWARD{})
+      fooType := reflect.TypeOf(huaweiV8R10_bgp.BACKWARD{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -149,40 +149,40 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-devm":
     switch splited[1] {
     case "devm/cpuInfos/cpuInfo":
-          fooType := reflect.TypeOf(huawei_devm.Devm_CpuInfos_CpuInfo{})
+          fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_CpuInfos_CpuInfo{})
           for i := 0; i < fooType.NumField(); i ++ {
             keys := fooType.Field(i)
             resolve[LcFirst(keys.Name)] = keys.Type
             }
         break;
     case "devm/fans/fan":
-      fooType := reflect.TypeOf(huawei_devm.Devm_Fans_Fan{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_Fans_Fan{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "devm/memoryInfos/memoryInfo":
-      fooType := reflect.TypeOf(huawei_devm.Devm_MemoryInfos_MemoryInfo{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_MemoryInfos_MemoryInfo{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "devm/ports/port":
-      fooType := reflect.TypeOf(huawei_devm.Devm_Ports_Port{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_Ports_Port{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "devm/ports/port/opticalInfo":
-      fooType := reflect.TypeOf(huawei_devm.Devm_Ports_Port{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_Ports_Port{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
-      fooType = reflect.TypeOf(huawei_devm.Devm_Ports_Port_OpticalInfo{})
+      fooType = reflect.TypeOf(huaweiV8R10_devm.Devm_Ports_Port_OpticalInfo{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "RxPower" || keys.Name == "TxPower" {
@@ -193,7 +193,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
         }
         break;
     case "devm/powerSupplys/powerSupply/powerEnvironments/powerEnvironment":
-      fooType := reflect.TypeOf(huawei_devm.Devm_PowerSupplys_PowerSupply_PowerEnvironments_PowerEnvironment{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_PowerSupplys_PowerSupply_PowerEnvironments_PowerEnvironment{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "PowerValue" || keys.Name == "VoltageValue" {
@@ -208,7 +208,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
         }
         break;
     case "devm/temperatureInfos/temperatureInfo":
-      fooType := reflect.TypeOf(huawei_devm.Devm_TemperatureInfos_TemperatureInfo{})
+      fooType := reflect.TypeOf(huaweiV8R10_devm.Devm_TemperatureInfos_TemperatureInfo{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "I2c" || keys.Name == "Channel" {
@@ -225,7 +225,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-driver":
     switch splited[1] {
     case "hwEntityInvalid":
-      fooType := reflect.TypeOf(huawei_driver.HwEntityInvalid{})
+      fooType := reflect.TypeOf(huaweiV8R10_driver.HwEntityInvalid{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "I2c" || keys.Name == "Channel" {
@@ -236,7 +236,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
         }
         break;
     case "hwEntityResume":
-      fooType := reflect.TypeOf(huawei_driver.HwEntityResume{})
+      fooType := reflect.TypeOf(huaweiV8R10_driver.HwEntityResume{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "I2c" || keys.Name == "Channel" {
@@ -247,7 +247,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
         }
         break;
     case "hwOpticalInvalid":
-      fooType := reflect.TypeOf(huawei_driver.HwOpticalInvalid{})
+      fooType := reflect.TypeOf(huaweiV8R10_driver.HwOpticalInvalid{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "I2c" || keys.Name == "Channel" {
@@ -258,7 +258,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
         }
         break;
     case "hwOpticalInvalidResume":
-      fooType := reflect.TypeOf(huawei_driver.HwOpticalInvalidResume{})
+      fooType := reflect.TypeOf(huaweiV8R10_driver.HwOpticalInvalidResume{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         if keys.Name == "I2c" || keys.Name == "Channel" {
@@ -273,7 +273,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
     return resolve
 
   case "huawei-ifm":
-    fooType := reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface{})
+    fooType := reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface{})
     for i := 0; i < fooType.NumField(); i ++ {
       keys := fooType.Field(i)
       if keys.Name == "IfName" {
@@ -282,35 +282,35 @@ func GetTypeValue (path string) map[string]reflect.Type {
       }
     switch splited[1] {
     case "ifm/interfaces/interface": // No trae data mas que IfIndex, IfName e IfAdminStatus_UP si la interface esta Down no devuevle el campo.
-      fooType = reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface{})
+      fooType = reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "ifm/interfaces/interface/ifClearedStat":
-      fooType := reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface_IfClearedStat{})
+      fooType := reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface_IfClearedStat{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "ifm/interfaces/interface/ifDynamicInfo":
-      fooType := reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface_IfDynamicInfo{})
+      fooType := reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface_IfDynamicInfo{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "ifm/interfaces/interface/ifStatistics":
-      fooType := reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface_IfStatistics{})
+      fooType := reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface_IfStatistics{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "ifm/interfaces/interface/ifStatistics/ethPortErrSts":
-      fooType := reflect.TypeOf(huawei_ifm.Ifm_Interfaces_Interface_IfStatistics_EthPortErrSts{})
+      fooType := reflect.TypeOf(huaweiV8R10_ifm.Ifm_Interfaces_Interface_IfStatistics_EthPortErrSts{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -320,7 +320,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
     return resolve
   case "huawei-isiscomm":
   case "huawei-isis":
-    fooType := reflect.TypeOf(huawei_isiscomm.IsisAdjacencyChange{})
+    fooType := reflect.TypeOf(huaweiV8R10_isiscomm.IsisAdjacencyChange{})
     for i := 0; i < fooType.NumField(); i ++ {
       keys := fooType.Field(i)
       resolve[LcFirst(keys.Name)] = keys.Type
@@ -328,7 +328,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
     return resolve
 
   case "huawei-mpls":
-    fooType := reflect.TypeOf(huawei_mpls.Mpls{})
+    fooType := reflect.TypeOf(huaweiV8R10_mpls.Mpls{})
     for i := 0; i < fooType.NumField(); i ++ {
       keys := fooType.Field(i)
       resolve[LcFirst(keys.Name)] = keys.Type
@@ -338,14 +338,14 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-ospfv2":
     switch splited[1] {
     case "ospfNbrStateChange":
-      fooType := reflect.TypeOf(huawei_ospfv2.OspfNbrStateChange{})
+      fooType := reflect.TypeOf(huaweiV8R10_ospfv2.OspfNbrStateChange{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
       break;
     case "ospfVirtNbrStateChange":
-      fooType := reflect.TypeOf(huawei_ospfv2.OspfVirtNbrStateChange{})
+      fooType := reflect.TypeOf(huaweiV8R10_ospfv2.OspfVirtNbrStateChange{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -355,7 +355,7 @@ func GetTypeValue (path string) map[string]reflect.Type {
     return resolve
 
   case "huawei-ospfv3":
-    fooType := reflect.TypeOf(huawei_ospfv3.Ospfv3NbrStateChange{})
+    fooType := reflect.TypeOf(huaweiV8R10_ospfv3.Ospfv3NbrStateChange{})
     for i := 0; i < fooType.NumField(); i ++ {
       keys := fooType.Field(i)
       resolve[LcFirst(keys.Name)] = keys.Type
@@ -365,21 +365,21 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-qos":
     switch splited[1] {
     case "qos/qosBuffers/qosBuffer":
-      fooType := reflect.TypeOf(huawei_qos.Qos_QosBuffers_QosBuffer{})
+      fooType := reflect.TypeOf(huaweiV8R10_qos.Qos_QosBuffers_QosBuffer{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "qos/qosIfQoss/qosIfQos/qosPolicyApplys/qosPolicyApply/qosPolicyStats/qosPolicyStat/qosRuleStats/qosRuleStat":
-      fooType := reflect.TypeOf(huawei_qos.Qos_QosIfQoss_QosIfQos_QosPolicyApplys_QosPolicyApply_QosPolicyStats_QosPolicyStat_QosRuleStats_QosRuleStat{})
+      fooType := reflect.TypeOf(huaweiV8R10_qos.Qos_QosIfQoss_QosIfQos_QosPolicyApplys_QosPolicyApply_QosPolicyStats_QosPolicyStat_QosRuleStats_QosRuleStat{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "qos/qosPortQueueStatInfos/qosPortQueueStatInfo":
-      fooType := reflect.TypeOf(huawei_qos.Qos_QosPortQueueStatInfos_QosPortQueueStatInfo{})
+      fooType := reflect.TypeOf(huaweiV8R10_qos.Qos_QosPortQueueStatInfos_QosPortQueueStatInfo{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -391,28 +391,28 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-sem":
     switch splited[1] {
     case "hwCPUUtilizationResume":
-      fooType := reflect.TypeOf(huawei_sem.HwStorageUtilizationResume{})
+      fooType := reflect.TypeOf(huaweiV8R10_sem.HwStorageUtilizationResume{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "hwCPUUtilizationRisingAlarm":
-      fooType := reflect.TypeOf(huawei_sem.HwCPUUtilizationRisingAlarm{})
+      fooType := reflect.TypeOf(huaweiV8R10_sem.HwCPUUtilizationRisingAlarm{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "hwStorageUtilizationResume":
-      fooType := reflect.TypeOf(huawei_sem.HwStorageUtilizationResume{})
+      fooType := reflect.TypeOf(huaweiV8R10_sem.HwStorageUtilizationResume{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
         break;
     case "hwStorageUtilizationRisingAlarm":
-      fooType := reflect.TypeOf(huawei_sem.HwStorageUtilizationRisingAlarm{})
+      fooType := reflect.TypeOf(huaweiV8R10_sem.HwStorageUtilizationRisingAlarm{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -425,14 +425,14 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-emdi":
     switch splited[1] {
     case "emdi/emdiTelemReps/emdiTelemRep":
-      fooType := reflect.TypeOf(huawei_telemEmdi.TelemEmdi_EmdiTelemReps_EmdiTelemRep{})
+      fooType := reflect.TypeOf(huaweiV8R10_telemEmdi.TelemEmdi_EmdiTelemReps_EmdiTelemRep{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
       break;
     case "emdi/emdiTelemRtps/emdiTelemRtp":
-      fooType := reflect.TypeOf(huawei_telemEmdi.TelemEmdi_EmdiTelemRtps_EmdiTelemRtp{})
+      fooType := reflect.TypeOf(huaweiV8R10_telemEmdi.TelemEmdi_EmdiTelemRtps_EmdiTelemRtp{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
@@ -444,27 +444,27 @@ func GetTypeValue (path string) map[string]reflect.Type {
   case "huawei-trafficmng":
     switch splited[1] {
     case "trafficmng/tmSlotSFUs/tmSlotSFU/sfuStatisticss/sfuStatistics":
-      fooType := reflect.TypeOf(huawei_trafficmng.Trafficmng{})
+      fooType := reflect.TypeOf(huaweiV8R10_trafficmng.Trafficmng{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
-      fooType = reflect.TypeOf(huawei_trafficmng.Trafficmng_TmSlotSFUs{})
+      fooType = reflect.TypeOf(huaweiV8R10_trafficmng.Trafficmng_TmSlotSFUs{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
-      fooType = reflect.TypeOf(huawei_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU{})
+      fooType = reflect.TypeOf(huaweiV8R10_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
-      fooType = reflect.TypeOf(huawei_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU_SfuStatisticss{})
+      fooType = reflect.TypeOf(huaweiV8R10_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU_SfuStatisticss{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
         }
-      fooType = reflect.TypeOf(huawei_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU_SfuStatisticss_SfuStatistics{})
+      fooType = reflect.TypeOf(huaweiV8R10_trafficmng.Trafficmng_TmSlotSFUs_TmSlotSFU_SfuStatisticss_SfuStatistics{})
       for i := 0; i < fooType.NumField(); i ++ {
         keys := fooType.Field(i)
         resolve[LcFirst(keys.Name)] = keys.Type
